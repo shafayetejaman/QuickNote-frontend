648c9053242511c0e7fb70afb37e3d283413d171
shafayetejaman <shafaitzaman@gmail.com>
2026-04-26 20:17:32 +0600

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { Routes, Route } from "react-router"
+
+import Home from "./component/Home.jsx"
+import Login from "./component/Login.jsx"
+import Register from "./component/Register.jsx"
+
 export default function App() {
-    return <h1 className="text-3xl font-bold text-green-950">Hello world!</h1>
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    )
 }
