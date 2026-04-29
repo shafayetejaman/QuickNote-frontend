c72fba2a43d76bb6b9a46bdf7d28e1bb398aba27
shafayetejaman <shafaitzaman@gmail.com>
2026-04-29 12:23:40 +0600

    start loging page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-    return <h1>Login</h1>
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="text-center p-8 rounded-xl border-2 border-slate-500">
+                <h1 className="text-3xl text-neutral-100 mb-3">Welcome Back</h1>
+                <h1 className="text-neutral-400">Sign in to continue</h1>
+            </div>
+        </div>
+    )
 }
