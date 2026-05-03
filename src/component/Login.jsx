f8a2df4a9d605d44ab1fc61903fa27efefb7aa5e
shafayetejaman <shafaitzaman@gmail.com>
2026-05-03 20:51:20 +0600

    login component complete

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,33 @@
+import axios from "axios"
 import { Link } from "react-router"
 
-export default function Home() {
-    function submit() {}
+async function handleLogin(username, password) {
+    console.log(username, password)
+    const env = import.meta.env
+
+    try {
+        const response = await axios.post(
+            `${env.VITE_LOCAL_BASE_BACKEND_URL}/api/v1/users/login`,
+            {
+                username,
+                password,
+            }
+        )
+
+        console.log(response.data)
+    } catch (error) {
+        const message = error.message || "Login failed"
+        throw new Error(message)
+    }
+}
+
+export default function Login() {
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        handleLogin(formData.get("username"), formData.get("password"))
+    }
+
     return (
         <div className="flex justify-center items-center h-screen m-2">
             <div className="text-neutral-100 max-w-screen-sm w-full p-8 rounded-lg border border-neutral-800 bg-neutral-950">
@@ -9,30 +35,38 @@ export default function Home() {
                     <h1 className="text-3xl mb-3">Welcome Back</h1>
                     <h1 className="text-neutral-400">Sign in to continue</h1>
                 </div>
-                <form onSubmit={submit} className="flex flex-col gap-3 mt-7">
+                <form
+                    onSubmit={onSubmit}
+                    method="POST"
+                    className="flex flex-col gap-3 mt-7"
+                >
                     <div>
-                        <h1 className="mb-1">Email</h1>
+                        <lable>Username</lable>
                         <input
-                            placeholder="Enter Your Email"
-                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                            type="username"
+                            name="username"
+                            placeholder="Enter Your Username"
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800 mt-1"
                         />
                     </div>
                     <div>
-                        <h1 className="mb-1">Password</h1>
+                        <lable>Password</lable>
                         <input
+                            type="password"
+                            name="password"
                             placeholder="Enter Your Password"
-                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800 mt-1"
                         />
                     </div>
                     <button className="w-full bg-red-600 rounded p-2 mt-2">
                         Sign In
                     </button>
-                    <h1 className="text-center text-neutral-400">
+                    <p className="text-center text-neutral-400">
                         Don't have an account?{" "}
                         <Link className="text-red-600" to="/register">
                             Sign Up
                         </Link>
-                    </h1>
+                    </p>
                 </form>
             </div>
         </div>
