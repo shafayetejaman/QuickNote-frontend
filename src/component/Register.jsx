9a959ef186bf2615500dfca6f163b30ab7c606c4
shafayetejaman <shafaitzaman@gmail.com>
2026-05-08 20:56:25 +0600

    add register page

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,3 +1,126 @@
-export default function Home() {
-    return <h1>register</h1>
+import axios from "axios"
+import { Link } from "react-router"
+import { useState } from "react"
+
+async function handleRegister(formData) {
+    const env = import.meta.env
+
+    try {
+        const response = await axios.post(
+            `${env.VITE_LOCAL_BASE_BACKEND_URL}/api/v1/users/register`,
+            formData,
+            {
+                headers: { "Content-Type": "multipart/form-data" },
+            }
+        )
+        console.log(response.data)
+    } catch (error) {
+        const message = error.message || "Register failed"
+        throw new Error(message)
+    }
+}
+
+export default function Register() {
+    const [loading, setLoading] = useState(false)
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+
+        setLoading(true)
+        try {
+            await handleRegister(formData)
+            // TODO: success handling
+        } catch (err) {
+            const message = error.message || "Register failed"
+            throw new Error(message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen m-2">
+            <div className="text-neutral-100 max-w-screen-sm w-full p-8 rounded-lg border border-neutral-800 bg-neutral-950">
+                <div className="text-center">
+                    <h1 className="text-3xl mb-3">Create Account</h1>
+                    <p className="text-neutral-400">Sign up to get started</p>
+                </div>
+
+                <form onSubmit={onSubmit} className="flex flex-col gap-3 mt-7">
+                    <div>
+                        <label className="block mb-1">Full Name</label>
+                        <input
+                            type="text"
+                            name="fullName"
+                            placeholder="ex: John Doe"
+                            required
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-1">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="ex: johndoe"
+                            required
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-1">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="ex: john@example.com"
+                            required
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-1">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="••••••••"
+                            required
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-1">Profile Image</label>
+                        <input
+                            type="file"
+                            name="profileImage"
+                            accept="image/*"
+                            className="w-full bg-[#171717] rounded p-2 border border-neutral-800 accent-gray-900"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="w-full bg-red-600 rounded p-2 mt-2 hover:bg-red-700 transition disabled:opacity-50"
+                    >
+                        {loading ? "Signing..." : "Sign Up"}
+                    </button>
+
+                    <p className="text-center text-neutral-400">
+                        Already have account?{" "}
+                        <Link
+                            className="text-red-600 hover:underline"
+                            to="/login"
+                        >
+                            Sign In
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
 }
