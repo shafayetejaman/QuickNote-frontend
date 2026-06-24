81d328c4770d1803a7c3133721d95693b5589c01
shafayetejaman <shafaitzaman@gmail.com>
2026-06-24 21:15:54 +0600

    ongling api

--- src/component/utils/api.js
+++ src/component/utils/api.js
@@ -1,12 +1,20 @@
 import axios from "axios"
 
 function initAxios() {
-    const axiosInstance = null
-    const baseUrl = env.VITE_LOCAL_BASE_BACKEND_URL | ""
+    const baseURL = env.VITE_LOCAL_BASE_BACKEND_URL | ""
+    const axiosInstance = axios.create({
+        baseURL,
+        timeout: 5000,
+    })
 
-    if (bseeUrl === "") {
+    if (baseURL === "") {
         throw Error("env not found")
     }
+
+    axiosInstance.interceptors.request.use((config) => {
+        // Do something before request is sent
+        return config
+    })
 
     return axiosInstance
 }
